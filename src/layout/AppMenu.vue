0343be665c3e72f99e9761d90dad4b25ad3d5f09
ali <you@example.com>
2023-07-06 14:10:09 +0330

    calender and admin dilog

--- src/layout/AppMenu.vue
+++ src/layout/AppMenu.vue
@@ -8,7 +8,6 @@ const model = ref([
         label: 'Home',
         items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' },
         { label: 'Users', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-        { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
         { label: 'Chart menu', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/chart' },
         { label: 'Upload', icon: 'pi pi-upload', to: '/uikit/file' },
         {label:'Animations' , icon :'pi pi-images' , to:'/pages/Crud'},
@@ -18,44 +17,18 @@ const model = ref([
     ]
 
     },
-    {
-        label: 'UI Components',
-        items: [
-            { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-            { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-            { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
-            { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
-            { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-            { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-            { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-            { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-            { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
 
-            { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
-            { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/messages' },
-            { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-            { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
+    {
+        items: [
         ]
     },
     {
         items: [
-            // { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: 'NEW' },
-            // { label: 'All Blocks', icon: 'pi pi-fw pi-globe', url: 'https://www.primefaces.org/primeblocks-vue', target: '_blank' }
         ]
     },
     {
         items: [
-            // { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },
-            // { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://www.primefaces.org/primeflex/', target: '_blank' }
-        ]
-    },
-    {
-        items: [
-            // {
-            //     label: 'Landing',
-            //     icon: 'pi pi-fw pi-globe',
-            //     to: '/landing'
-            // },
+    
             {
                 label: 'Auth',
                 icon: 'pi pi-fw pi-user',
@@ -77,90 +50,14 @@ const model = ref([
                     }
                 ]
             },
-            // {
-            //     label: 'Crud',
-            //     icon: 'pi pi-fw pi-pencil',
-            //     to: '/pages/cruds'
-            // },
-            // {
-            //     label: 'Timeline',
-            //     icon: 'pi pi-fw pi-calendar',
-            //     to: '/pages/timeline'
-            // },
-            // {
-            //     label: 'Not Found',
-            //     icon: 'pi pi-fw pi-exclamation-circle',
-            //     to: '/pages/notfound'
-            // },
-            // {
-            //     label: 'Empty',
-            //     icon: 'pi pi-fw pi-circle-off',
-            //     to: '/pages/empty'
-            // }
+
         ]
     },
     {
-        // items: [
-        //     {
-        //         label: 'Submenu 1',
-        //         icon: 'pi pi-fw pi-bookmark',
-        //         items: [
-        //             {
-        //                 label: 'Submenu 1.1',
-        //                 icon: 'pi pi-fw pi-bookmark',
-        //                 items: [
-        //                     { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-        //                     { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-        //                     { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-        //                 ]
-        //             },
-        //             {
-        //                 label: 'Submenu 1.2',
-        //                 icon: 'pi pi-fw pi-bookmark',
-        //                 items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-        //             }
-        //         ]
-        //     },
-        //     {
-        //         label: 'Submenu 2',
-        //         icon: 'pi pi-fw pi-bookmark',
-        //         items: [
-        //             {
-        //                 label: 'Submenu 2.1',
-        //                 icon: 'pi pi-fw pi-bookmark',
-        //                 items: [
-        //                     { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-        //                     { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-        //                 ]
-        //             },
-        //             {
-        //                 label: 'Submenu 2.2',
-        //                 icon: 'pi pi-fw pi-bookmark',
-        //                 items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-        //             }
-        //         ]
-        //     }
-        // ]
+
     },
     {
-        // items: [
-        //     {
-        //         label: 'Documentation',
-        //         icon: 'pi pi-fw pi-question',
-        //         to: '/documentation'
-        //     },
-        //     {
-        //         label: 'View Source',
-        //         icon: 'pi pi-fw pi-search',
-        //         url: 'https://github.com/primefaces/sakai-vue',
-        //         target: '_blank'
-        //     },
-        //     {
-        //         label: 'Nuxt Version',
-        //         url: 'https://github.com/primefaces/sakai-nuxt',
-        //         icon: 'pi pi-fw pi-star'
-        //     }
-        // ]
+
     }
 ]);
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 
 import PrimeVue from 'primevue/config';
+
 import AutoComplete from 'primevue/autocomplete';
 import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
